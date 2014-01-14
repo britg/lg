e412073b6a038657b991f2f97cc830a94bcfd371
britg <brit@britg.com>
2014-01-13 20:08:39 -0600

    Camera follows player, top view

---
+++ Assets/Scripts/Camera/TopFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TopFollow : MonoBehaviour {
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position;
+		pos.x = player.transform.position.x;
+		pos.z = player.transform.position.z;
+
+		transform.position = pos;
+	}
+}
