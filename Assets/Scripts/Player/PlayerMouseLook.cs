7118045a00157bba3a6a58b00463ec7841de06bf
britg <brit@britg.com>
2014-01-25 17:43:23 -0600

    Move the retical with the right thumbstick

--- Assets/Scripts/Player/PlayerMouseLook.cs
+++ Assets/Scripts/Player/PlayerMouseLook.cs
@@ -6,16 +6,20 @@ public class PlayerMouseLook : MonoBehaviour {
 
 	private Vector2 playerScreenPos;
 	private VectorLine pointer;
+	private Vector2 lastMousePos;
+
 
 	// Use this for initialization
 	void Start () {
 		AssignPlayerPos();
 		CreatePointer();
+		lastMousePos = Vector2.zero;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		PointAtMouse();
+		FollowThumbstick();
 	}
 
 	void AssignPlayerPos () {
@@ -29,9 +33,24 @@ public class PlayerMouseLook : MonoBehaviour {
 	}
 
 	void PointAtMouse () {
-		Vector2 mousePosition = Input.mousePosition;
-		Vector2 towards = (mousePosition - playerScreenPos) * 100;
+		Vector2 mousePos = Input.mousePosition;
+
+		if (lastMousePos.Equals(mousePos)) {
+			return;
+		}
+
+		Vector2 towards = (mousePos - playerScreenPos) * 100;
 		pointer.points2[1] = playerScreenPos + towards;
 		pointer.Draw();
+
+		lastMousePos = mousePos;
+	}
+
+	void FollowThumbstick () {
+		Vector2 lookDirection = new Vector2(Input.GetAxis("Look X"), -Input.GetAxis("Look Y")).normalized * 1000;
+		if (lookDirection.sqrMagnitude > 0f) {
+			pointer.points2[1] = playerScreenPos + lookDirection;
+			pointer.Draw();
+		}
 	}
 }
